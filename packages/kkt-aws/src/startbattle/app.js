77f9afde1172fa2bf406035476d842603cdeb509
Chris Lee <cmlee1014@gmail.com>
2019-12-16 15:35:42 -0500

    Parse body, as it comes in as a string

--- packages/kkt-aws/src/startbattle/app.js
+++ packages/kkt-aws/src/startbattle/app.js
@@ -2,17 +2,26 @@
 import { start as startBattle } from 'kkt-battle';
 
 const handler = async (event) => {
-  const { body: { config: gameConfig } = {} } = event;
-  gameConfig.botConfigs.push({ color: 'green', strategy: { type: 'offline' } });
+  const { body = '{}' } = event;
+  try {
+    const { config: gameConfig = { botConfigs: [] } } = JSON.parse(body);
+    gameConfig.botConfigs.push({
+      color: 'green',
+      strategy: { type: 'offline' },
+    });
 
-  const state = await startBattle({ gameConfig });
-  const { id } = state;
+    const state = await startBattle({ gameConfig });
+    const { id } = state;
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ id, state }),
-  };
-  return response;
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ id, state }),
+    };
+    return response;
+  } catch (e) {
+    console.error(e);
+    return { statusCode: 500, body: JSON.stringify('Error starting battle') };
+  }
 };
 
 export { handler };
