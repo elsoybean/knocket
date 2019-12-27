6214a067fc48366693a61b287fe839b7ace6b8f5
Chris Lee <christopher.lee@envolvehealth.com>
2019-12-27 17:42:05 -0500

    Fix TTY frontend

--- packages/kkt-frontend-tty/src/index.js
+++ packages/kkt-frontend-tty/src/index.js
@@ -2,12 +2,13 @@
 
 import EventEmitter from 'events';
 import _ from 'lodash';
+import { start as startBattle, battleCycle, executeMove } from 'kkt-battle';
+import { applyToState } from 'kkt-battle-events';
 
 import type {
   Point,
   GameState,
   Bot,
-  Frontend,
 } from '../../kkt-battle/types/GameState.types';
 
 const HEADING_ARROWS: Array<string> = ['↖', '↗', '→', '↘', '↙', '←'];
@@ -46,102 +47,132 @@ const displayPoint = (p: Point, state: GameState): string => {
 const sleep = (ms: number) =>
   new Promise<void>((resolve) => setTimeout(resolve, ms));
 
-const ttyFrontend = (): Frontend => {
-  let modelMessage = '';
-  let quitting = false;
+let modelMessage = '';
+let keepRunning = true;
 
-  const render = async (state: GameState): Promise<void> => {
-    process.stdout.write('\u001b[2J\u001b[0;0H');
-    const { field, history, bots } = state;
-    const fieldSize = (3 + Math.sqrt(9 + 12 * (field.length - 1))) / 6;
-    for (let x = -1 * (fieldSize - 1); x < fieldSize; x++) {
-      const row = field.filter((p) => p.x === x) || [];
-      const spaces = (fieldSize - 1) * 2 + row[0].y - row[0].z;
-      process.stdout.write(
-        ' '.repeat(spaces) +
-          row.map((p) => displayPoint(p, state)).join(' ') +
-          '\n',
-      );
-    }
-    process.stdout.write('\n\n');
-    bots.forEach((b) => {
-      process.stdout.write(COLORS[b.color]);
-      process.stdout.write(b.health + '  ');
-      history
-        .filter((h) => h.botId == b.id)
-        .slice(0, 100)
-        .forEach((h) => {
-          let { type } = h;
-          if (type == 'rotate') {
-            type += h.clockwise ? 'cw' : 'ccw';
-          }
-          process.stdout.write(MOVE_ICONS[type]);
-        });
-      process.stdout.write('\x1b[0m\n');
-    });
-
-    process.stdout.write(modelMessage + '\n');
-
-    if (!quitting) {
-      await sleep(150);
-    }
-  };
-
-  const events = new EventEmitter();
-
-  events.on('init', () => {
-    process.stdin.resume();
-    // $FlowFixMe - this works
-    process.stdin.setRawMode(true);
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('data', (key: string) => {
-      if (key == 'q' || key === '\u0003') {
-        events.emit('quit');
-        events.emit('input', 'quit');
-      } else if (key === '\u001b[A') {
-        events.emit('input', 'ahead');
-      } else if (key === '\u001b[B') {
-        events.emit('input', 'reverse');
-      } else if (key === ' ') {
-        events.emit('input', 'attack');
-      } else if (key === '\u001b[C') {
-        events.emit('input', 'rotate', true);
-      } else if (key === '\u001b[D') {
-        events.emit('input', 'rotate', false);
-      } else if (key === 'x') {
-        events.emit('input', 'defend');
-      }
-    });
+const render = async (state: GameState): Promise<void> => {
+  process.stdout.write('\u001b[2J\u001b[0;0H');
+  const { field, history, bots } = state;
+  const fieldSize = (3 + Math.sqrt(9 + 12 * (field.length - 1))) / 6;
+  for (let x = -1 * (fieldSize - 1); x < fieldSize; x++) {
+    const row = field.filter((p) => p.x === x) || [];
+    const spaces = (fieldSize - 1) * 2 + row[0].y - row[0].z;
+    process.stdout.write(
+      ' '.repeat(spaces) +
+        row.map((p) => displayPoint(p, state)).join(' ') +
+        '\n',
+    );
+  }
+  process.stdout.write('\n\n');
+  bots.forEach((b) => {
+    process.stdout.write(COLORS[b.color]);
+    process.stdout.write(b.health + '  ');
+    history
+      .filter((h) => h.botId == b.id)
+      .slice(0, 100)
+      .forEach((h) => {
+        let { type } = h;
+        if (type == 'rotate') {
+          type += h.clockwise ? 'cw' : 'ccw';
+        }
+        process.stdout.write(MOVE_ICONS[type]);
+      });
+    process.stdout.write('\x1b[0m\n');
   });
 
-  events.on('win', (winner: Bot, state: GameState) => {
-    const { elapsed } = state;
-    // eslint-disable-next-line no-console
-    console.log(winner.color, 'has won! It took', elapsed);
-  });
+  process.stdout.write(modelMessage + '\n');
 
-  events.on('draw', () => {
-    // eslint-disable-next-line no-console
-    console.log('The game ended in a draw :/');
-  });
-
-  events.on('done', process.exit);
-
-  events.on('quit', () => {
-    quitting = true;
-  });
-
-  events.on('error', (err) => {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    process.exit();
-  });
-
-  events.on('model-data', (message) => {
-    modelMessage = message;
-  });
-
-  return { events, render };
+  if (keepRunning) {
+    await sleep(150);
+  }
 };
 
-export default ttyFrontend;
+const events = new EventEmitter();
+
+process.stdin.resume();
+// $FlowFixMe - this works
+process.stdin.setRawMode(true);
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (key: string) => {
+  if (key == 'q' || key === '\u0003') {
+    events.emit('quit');
+    events.emit('input', { type: 'quit' });
+  } else if (key === '\u001b[A') {
+    events.emit('input', { type: 'ahead' });
+  } else if (key === '\u001b[B') {
+    events.emit('input', { type: 'reverse' });
+  } else if (key === ' ') {
+    events.emit('input', { type: 'attack' });
+  } else if (key === '\u001b[C') {
+    events.emit('input', { type: 'rotate', options: { clockwise: true } });
+  } else if (key === '\u001b[D') {
+    events.emit('input', { type: 'rotate', options: { clockwise: false } });
+  } else if (key === 'x') {
+    events.emit('input', { type: 'defend' });
+  }
+});
+
+events.on('quit', () => {
+  keepRunning = false;
+});
+
+events.on('model-data', (message) => {
+  modelMessage = message;
+});
+
+const botConfigs: Array<BotConfig> = [
+  { color: 'green', strategy: { type: 'offline', options: { handle: 'tty' } } },
+  { color: 'red', strategy: { type: 'hunter' } },
+  { color: 'blue', strategy: { type: 'lump' } },
+];
+
+const options = {
+  gameConfig: { fieldSize: 4, botConfigs },
+};
+
+(async () => {
+  const state = await startBattle(options);
+  while (keepRunning) {
+    render(state);
+    const cyclePromise = new Promise((resolve) => {
+      const publishMove = (activeBot, move) => {
+        if (keepRunning) {
+          const historyItem = executeMove(activeBot, move, state);
+          applyToState(state, historyItem);
+        }
+        resolve();
+      };
+
+      const collectMove = (activeHandle, _sensorData_) => {
+        events.once('input', (move) => {
+          const { bots } = state;
+          const bot = bots.find(
+            ({ strategyConfig: { options: { handle } = {} } = {} }) =>
+              activeHandle === handle,
+          );
+          publishMove(bot, move);
+        });
+      };
+
+      const broadcastResult = ({ result, state, bot: winner }) => {
+        if (result === 'win') {
+          const { elapsed } = state;
+          console.log(winner.color, 'has won! It took', elapsed);
+        } else {
+          console.log('The game ended in a draw :/');
+        }
+        keepRunning = false;
+        resolve();
+      };
+
+      battleCycle(state, {
+        collectMove,
+        publishMove,
+        broadcastResult,
+      });
+    });
+    await cyclePromise;
+  }
+  process.stdin.pause();
+  render(state);
+})();
